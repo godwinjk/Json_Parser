3fb726e8229b07e21cb89f762b977847fd7e4139
GitHub Action <action@github.com>
2022-03-24 14:11:04 +0000

    Template cleanup

--- src/main/kotlin/com/github/godwinjk/jsonparser/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/godwinjk/jsonparser/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.godwinjk.jsonparser.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.godwinjk.jsonparser.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/godwinjk/jsonparser/services/MyProjectService.kt
+++ src/main/kotlin/com/github/godwinjk/jsonparser/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.godwinjk.jsonparser.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.godwinjk.jsonparser.MyBundle
 
 class MyProjectService(project: Project) {
 
