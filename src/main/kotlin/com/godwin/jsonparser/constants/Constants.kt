eb191e7cf2d4d73eb7d84e59da95557505c12080
Godwin Joseph <godwinjoseph.k@gmail.com>
2024-12-08 16:53:28 +0000

    notification update

--- src/main/kotlin/com/godwin/jsonparser/constants/Constants.kt
+++ src/main/kotlin/com/godwin/jsonparser/constants/Constants.kt
@@ -2,3 +2,4 @@ package com.godwin.jsonparser.constants
 
 const val TOOL_WINDOW_ID = "Json Parser"
 const val PLUGIN_NAME = "Json Parser"
+const val NOTIFICATION_GROUP_ID = "Json_Parser_Notification"
