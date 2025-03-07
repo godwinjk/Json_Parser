9ec84cf55c7e4d5fe339567765ad269d3b898f7c
Adekola Akano <akano.adekola@gmail.com>
2025-03-07 23:34:37 +0000

    Added more JsonUtils Tests

--- src/test/java/jsonparser/util/JsonUtilsTest.java
+++ src/test/java/jsonparser/util/JsonUtilsTest.java
@@ -8,8 +8,14 @@ import static org.junit.Assert.assertEquals;
 public class JsonUtilsTest {
 
     @Test
-    public void testValidJsonRemainsUnchanged() {
+    public void testValidJson_remainsUnchanged() {
         String json = "{\"first_Name\": \"John\", \"last_Name\": \"Doe\",\"Age\": 20}";
+        assertEquals(json, JsonUtils.cleanUpJsonString(json));
+    }
+
+    @Test
+    public void testValidJson_containsBraceInElement_remainsUnchanged() {
+        String json = "{\"first_Name\": \"Joh}}n\", \"last{_Name\": \"Doe\",\"Age\": 20}";
         assertEquals(json, JsonUtils.cleanUpJsonString(json));
     }
 
@@ -20,11 +26,26 @@ public class JsonUtilsTest {
         assertEquals(expected, JsonUtils.cleanUpJsonString(json));
     }
 
+    @Test
+    public void testMissingBothBraces_containsBraceInElement() {
+        String json = "\"first_N{ame\": \"John\", \"last_N{{ame\": \"Do}e\",\"Age\": 20";
+        String expected = "{\"first_N{ame\": \"John\", \"last_N{{ame\": \"Do}e\",\"Age\": 20}";
+        assertEquals(expected, JsonUtils.cleanUpJsonString(json));
+    }
+
     //    A Brace is present at the closing, but it's not the closing brace
     @Test
     public void testMissingBothBraces_where_aClosingBraceIsPresent() {
         String json = "\"first_Name\":\"John\",\"last_Name\":\"Doe\",\"Age\":20,\"department\":{\"name\":\"sales\"}";
         String expected = "{\"first_Name\":\"John\",\"last_Name\":\"Doe\",\"Age\":20,\"department\":{\"name\":\"sales\"}}";
+        assertEquals(expected, JsonUtils.cleanUpJsonString(json));
+    }
+
+    //    A Brace is present at the closing, but it's not the closing brace
+    @Test
+    public void testMissingBothBraces_containsBraceInElement_where_aClosingBraceIsPresent() {
+        String json = "\"first_Name\":\"John\",\"last_Name\":\"Doe}}\",\"Age{\":20,\"department\":{\"name\":\"sales\"}";
+        String expected = "{\"first_Name\":\"John\",\"last_Name\":\"Doe}}\",\"Age{\":20,\"department\":{\"name\":\"sales\"}}";
         assertEquals(expected, JsonUtils.cleanUpJsonString(json));
     }
 
